e569a59a2b857102246cf358bfc7853b65eed66c
Feldwor <BoQsc@users.noreply.github.com>
2020-09-25 10:14:05 +0300

    Create RegGetValueAPathEnvironmentVariable.c

---
+++ Tiny C compiler/learningWinapi/RegGetValueAPathEnvironmentVariable.c
@@ -0,0 +1,52 @@
+#include <stdio.h>   // Prevents a warning:                  test.c:34: warning: implicit declaration of function 'printf'
+#include <windows.h> // Required, else an error:             test.c:12: error: 'DWORD' undeclared
+
+/* 
+Useful information 
+   Function:  RegGetValueA
+   Reference: https://docs.microsoft.com/en-us/windows/win32/api/winreg/nf-winreg-reggetvaluea
+
+ Proper compilation method/tutorial: 
+   tcc -impdef advapi32.dll -o .\lib\advapi32.def
+   tcc -ladvapi32 test.c
+
+More Documentation
+   System Path variable locations
+   https://stackoverflow.com/questions/573817/where-are-environment-variables-stored-in-registry/573818#573818
+
+   User Variables
+   HKEY_CURRENT_USER\Environment
+
+   System Variables
+   Computer\HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment
+
+*/
+
+
+
+void main(){
+	char value[255];
+	
+	#define BUFFER  8192
+	DWORD BufferSize = BUFFER;
+	
+	HKEY    hkey     = HKEY_LOCAL_MACHINE;
+	LPCSTR  lpSubKey = "SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment";
+	LPCSTR  lpValue  = "Path";
+	DWORD   dwFlags  = RRF_RT_ANY;
+	LPDWORD pdwType  = NULL;
+	PVOID   pvData   = &value;
+	LPDWORD pcbData  = &BufferSize;
+	
+	RegGetValueA(
+		hkey,
+		lpSubKey,
+		lpValue,
+		dwFlags,
+		pdwType,
+		pvData,
+		pcbData
+	);
+	
+	printf("\n%s\n", value);
+}
