3e3b46233d6508ceba66bdc9f0354bb1d6cf64b3
Feldwor <BoQsc@users.noreply.github.com>
2020-09-25 11:58:01 +0300

    Add files via upload

---
+++ FindAndReplaceString.c
@@ -0,0 +1,76 @@
+// C program to search and replace 
+// all occurrences of a word with 
+// other word. https://www.geeksforgeeks.org/c-program-replace-word-text-another-given-word/
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+  
+// Function to replace a string with another 
+// string 
+
+
+char* replaceWord(const char* stringInput, 
+                  const char* originalWord, 
+                  const char* replacementWord) 
+{ 
+	char* result; 
+	
+    int replacementWordLength = strlen(replacementWord); 
+    int originalLenght = strlen(originalWord); 
+	const char terminationCharacter = '\0';	
+	
+	
+    // Counting the number of times old word 
+    // occur in the string
+    int i;
+	int occurences;
+    for (i = 0, occurences=0; stringInput[i] != terminationCharacter; i++) { 
+        if (strstr(&stringInput[i], originalWord) == &stringInput[i]) { 
+            occurences++; 
+  
+            // Jumping to index after the old word. 
+            i += originalWordLenght - 1; 
+        } 
+    } 
+  
+    //-------------------------------------------------------
+  
+    // Making new string of enough length 
+    result = (char*)malloc(
+						   i + occurences * (replacementWordLength - originalWordLenght) + 1
+	 ); 
+  
+    i = 0; 
+    while (*stringInput) { 
+        // compare the substring with the result 
+        if (strstr(stringInput, originalWord) == stringInput) { 
+            strcpy(&result[i], replacementWord); 
+            i += replacementWordLength; 
+            stringInput += originalWordLenght; 
+        } 
+        else
+            result[i++] = *stringInput++; 
+    } 
+  
+    result[i] = terminationCharacter; 
+    return result; 
+} 
+  
+// Driver Program 
+int main() 
+{ 
+    char input[] = "xxforxx xx for xx"; 
+    char word[] = "xx"; 
+    char replacement[] = "Geeks"; 
+  
+    char* result = NULL; 
+  
+    // originalWord string 
+    printf("Old string: %s\n", input); 
+  
+    result = replaceWord(input, word, replacement); 
+    printf("New String: %s\n", result); 
+  
+    free(result); 
+    return 0; 
+} 
