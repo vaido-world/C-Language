f5095c17c9902b0adc8255925f1d1de2b4769ec2
Feldwor <BoQsc@users.noreply.github.com>
2020-10-07 14:08:39 +0300

    Update loopMenuOptions3.cmd

--- Tiny C compiler/rewrite-build-tcc/loopMenuOptions3.cmd
+++ Tiny C compiler/rewrite-build-tcc/loopMenuOptions3.cmd
@@ -8,8 +8,8 @@ REM Previous option was -c, accept as argument of -c
 FOR %%G IN (%*) DO ( 
 	SET "argument=%%G"
 	IF NOT "!argument:~0,1!"=="-" ECHO not argument
-	IF "%argument%"=="-c" SET "compile=y"
+	IF "!argument!"=="-c" SET "compile=y"
 	
-	IF "%argument%"=="-c" SET "compile=y"
+	IF "!argument!"=="-c" SET "compile=y"
 )
 
