c4a4fa71afd2c06ac953cba75ed1bb2f7a3ec2b3
Feldwor <BoQsc@users.noreply.github.com>
2020-10-13 10:25:25 +0300

    Update RegGetValueAPathEnvironmentVariable.c

--- Tiny C compiler/learningWinapi/RegGetValueAPathEnvironmentVariable.c
+++ Tiny C compiler/learningWinapi/RegGetValueAPathEnvironmentVariable.c
@@ -1,13 +1,12 @@
 #pragma comment(lib, "advapi32") // No more Need for tcc -ladvapi32 RegGetValueA.c         tcc: error: undefined symbol 'RegGetValueA' 
-#include <stdio.h>               // Prevents a warning:                                    test.c:34: warning: implicit declaration of function 'printf'
-#include <windows.h>             // Required, else an error:                               test.c:12: error: 'DWORD' undeclared
-
+#include <stdio.h>               // Prevents a warning:                  test.c:34: warning: implicit declaration of function 'printf'
+#include <windows.h>             // Required, else an error:             test.c:12: error: 'DWORD' undeclared
 /* 
 Useful information 
    Function:  RegGetValueA
    Reference: https://docs.microsoft.com/en-us/windows/win32/api/winreg/nf-winreg-reggetvaluea
    
-                                  // Required, else an error:                               tcc: error: undefined symbol 'RegGetValueA'
+                                 //Required, else an error:               tcc: error: undefined symbol 'RegGetValueA'
  Proper compilation method/tutorial 
    tcc -impdef advapi32.dll -o .\lib\advapi32.def
    tcc -ladvapi32 RegGetValueAPathEnvironmentVariable.c
@@ -28,13 +27,15 @@ More Documentation
 
 void main(){
 	
-	// Notice: the program silently crashes if there are not enough characters allocated
-	// https://docs.microsoft.com/en-us/windows/win32/seccrypto/retrieving-data-of-unknown-length
-	// https://stackoverflow.com/questions/9137157/no-out-of-bounds-error/9137188#9137188
-	char value[2550];
+	
+
+	
+	// Notice: the program silently crashes if there is not enough characters allocated
+	char value[2000];
 	
 	#define BUFFER  8192
 	DWORD BufferSize = BUFFER;
+	
 	
 	HKEY    hkey     = HKEY_LOCAL_MACHINE;
 	LPCSTR  lpSubKey = "SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment";
@@ -54,5 +55,25 @@ void main(){
 		pcbData
 	);
 	
-	printf("\n%s\n", value);
+	
+	if (BufferSize >= strlen(value)) {
+		printf("                  ---");
+		printf("\n");
+		printf("%s", value);
+		printf("\n");
+		printf("                  ---");
+		printf("\n");
+
+		printf("Retrieved information about array size: %i\n",BufferSize);
+		printf("Actual size of the array: %i\n", strlen(value));
+		printf("Manually Allocated bytes for the array: %i", sizeof(value));
+	
+	} else {
+		
+		// This is not going to be seen, 
+		// the program crashes when the array is too small, therefore out of bounds
+		// printf("Is too small");
+	}
+		
+
 }
