73362e63c262860e46914c676a2dc962a04047f9
Feldwor <BoQsc@users.noreply.github.com>
2020-11-09 15:19:25 +0200

    Add files via upload

---
+++ examples/header_example/myfun.h
@@ -0,0 +1,11 @@
+/*define macro MY_FUN, this will reduce multiple calling
+  of the header file*/
+   
+#ifndef MY_FUN
+    #define MY_FUN
+     
+    /*function declarations*/
+    int sumOfNumbers(int x,int y);
+    float   avgOfNumbers(int x,int y);
+ 
+#endif
