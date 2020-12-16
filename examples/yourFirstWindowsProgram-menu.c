b2ed6b4169c48fa10bba3228382eecfa04070fc4
Feldwor <BoQsc@users.noreply.github.com>
2020-12-16 14:12:03 +0200

    Update yourFirstWindowsProgram-menu.c

--- examples/yourFirstWindowsProgram-menu.c
+++ examples/yourFirstWindowsProgram-menu.c
@@ -146,14 +146,14 @@ CALLBACK WindowProcedure(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			
 			// Client area Icon
 			DrawIconEx( hdc,        // Handle for Device Context
-						100,        // xLeft
-						200,        // yTop
-						hIcon,      // hIcon
-						72,         // cxWidth
-						78,         // cyWidth
-						0,          // istepIfAniCur
-						NULL,       // hbrFlickerFreeDraw
-						DI_NORMAL); // diFlags
+                        100,        // xLeft
+                        200,        // yTop
+                        hIcon,      // hIcon
+                        72,         // cxWidth
+                        78,         // cyWidth
+                        0,          // istepIfAniCur
+                        NULL,       // hbrFlickerFreeDraw
+                        DI_NORMAL); // diFlags
 
 			EndPaint(hwnd, &ps);
 			}
