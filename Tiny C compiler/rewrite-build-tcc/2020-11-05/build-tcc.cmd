bbcaccaf4cfd2c79c5cb1d413a0ffd863325010c
Feldwor <BoQsc@users.noreply.github.com>
2021-01-04 14:28:31 +0200

    Update build-tcc.cmd

--- Tiny C compiler/rewrite-build-tcc/2020-11-05/build-tcc.cmd
+++ Tiny C compiler/rewrite-build-tcc/2020-11-05/build-tcc.cmd
@@ -12,7 +12,7 @@ pause
 
 IF "%1"=="" (
 	WHERE "gcc.exe" >NUL 2>NUL 
-	IF ERRORLEVEL 0 (
+	IF ERRORLEVEL 01 (
 		ECHO              DEFAULT COMPILER will be used
 		ECHO          GCC is the suggested default compiler
 		ECHO __________________________________________________________
