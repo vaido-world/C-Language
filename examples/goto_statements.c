2362f17f821db5eaa6632637c75f794f22b6c4e0
Feldwor <BoQsc@users.noreply.github.com>
2021-01-08 14:11:07 +0200

    Add files via upload

---
+++ examples/goto_statements.c
@@ -0,0 +1,8 @@
+void main(){
+______________Initialization______________:
+	printf("Information and testing");
+	gets();
+	
+__________________Ending__________________:
+     printf("Ending the program");
+}
