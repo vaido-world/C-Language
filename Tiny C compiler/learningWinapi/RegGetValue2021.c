dea4ced9decc827f609d48f7a29dfddfebc126bf
Feldwor <BoQsc@users.noreply.github.com>
2021-06-07 10:40:27 +0300

    Update RegGetValue2021.c

--- Tiny C compiler/learningWinapi/RegGetValue2021.c
+++ Tiny C compiler/learningWinapi/RegGetValue2021.c
@@ -102,6 +102,7 @@ void getRegistryKeyValue(){
 					
 
 		// Holds size of the buffer for pvData value in bytes
+		// Variable is overwritten when returning, so making a macro that holds the initial value would be needed.
 		// https://stackoverflow.com/questions/26069085/visual-c-reggetvalue-fails-in-program-where-it-shouldnt/26069277#26069277
 		DWORD BufferSize;
 		
