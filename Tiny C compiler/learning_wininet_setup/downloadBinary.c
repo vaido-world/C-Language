10d014bb2efde6834ec152eb52120518e5764ea8
Feldwor <BoQsc@users.noreply.github.com>
2021-06-20 20:34:32 +0300

    Add files via upload

---
+++ Tiny C compiler/learning_wininet_setup/downloadBinary.c
@@ -0,0 +1,53 @@
+#include <windows.h>
+#include <wininet.h>
+#include <stdio.h>
+#pragma comment(lib,"wininet")
+
+
+int main(int argc, char* argv[])
+{
+HINTERNET hInternetSession;
+HINTERNET hURL;
+            // I'm only going to access 1K of info.
+BOOL bResult;
+DWORD dwBytesRead=1;
+
+// Make internet connection.
+hInternetSession = InternetOpen(
+    TEXT("tesg"), // agent
+    INTERNET_OPEN_TYPE_PRECONFIG,  // access
+    NULL, NULL, 0);                // defaults
+
+// Make connection to desired page.
+hURL = InternetOpenUrl(
+    hInternetSession,                       // session handle
+    TEXT("http://wallpapers-images.ru/1920x1080/nature/wallpapers/wallpapers-nature-1.jpg"),  // URL to access
+    NULL, 0, 0, 0);                         // defaults
+
+// Read page into memory buffer.
+
+char buf[1024];
+
+DWORD dwTemp;
+HANDLE hFile = CreateFile(TEXT("пример.jpg"), GENERIC_WRITE, 0, NULL,
+    CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+
+if (INVALID_HANDLE_VALUE == hFile) {
+    return 0;
+}
+
+for (;dwBytesRead>0;)
+{
+    InternetReadFile(hURL, buf, (DWORD)sizeof(buf), &dwBytesRead);
+    WriteFile(hFile, buf, dwBytesRead, &dwTemp, NULL);
+}
+
+// Close down connections.
+InternetCloseHandle(hURL);
+InternetCloseHandle(hInternetSession);
+
+CloseHandle(hFile);
+
+
+return 0;
+}
