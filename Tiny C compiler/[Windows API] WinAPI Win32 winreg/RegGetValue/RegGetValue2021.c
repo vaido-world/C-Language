ec342fc6c4f358423168c76f0cbd23646825885f
Feldwor <BoQsc@users.noreply.github.com>
2021-06-27 20:23:51 +0300

    Update RegGetValue2021.c

--- Tiny C compiler/[Windows API] WinAPI Win32 winreg/RegGetValue/RegGetValue2021.c
+++ Tiny C compiler/[Windows API] WinAPI Win32 winreg/RegGetValue/RegGetValue2021.c
@@ -1,6 +1,3 @@
-// RegGetValue.c 
-// Receives registry value from the Registry key
-
 /*
 This work is marked with CC0 1.0 Universal. 🄍
 
@@ -10,6 +7,9 @@ upon the material in any medium or format, even for commercial purposes.
 
 To view a copy of this license, visit http://creativecommons.org/publicdomain/zero/1.0
 */
+
+// RegGetValue.c 
+// Receives registry value from the Registry key
 
 // Compiler Pragmatic comments
 #pragma comment(lib, "advapi32") // No more Need for "tcc -ladvapi32 RegGetValueA.c"         tcc: error: undefined symbol 'RegGetValueA' 
